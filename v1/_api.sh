a30a889b9ea43de302c2a810bb3a820ad969405e
James Harris <mailjamesharris@gmail.com>
2019-08-28 10:44:33 +1000

    Move files out of `make-resources` directory.

---
+++ v1/_api.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+case $1 in
+    spec-version)
+        echo 1
+        ;;
+    version)
+        echo 1
+        ;;
+    *)
+        echo "unknown command '$1'"
+        exit 1
+esac
