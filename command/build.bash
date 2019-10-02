95d4fdd35cd2935094cebd5d96b4cb5759a46333
James Harris <mailjamesharris@gmail.com>
2019-10-02 10:42:48 +1000

    Add a `build` command to download and install `make-` packages.

--- command/build.bash
+++ command/build.bash
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 echo "BUILDING $1"
+TARGET="$1"
+
+if ! [[ $TARGET =~ ^\.makefiles/(.+)/v([0-9]+)/ ]]; then
+    2>&1 echo "can not build '$TARGET', target names must be of the form '.makefiles/<package>/v<version>/...'."
+    exit 1
+fi
+
+REPO="${BASH_REMATCH[1]}"
+VER="${BASH_REMATCH[2]}"
+ARCHIVE=$("$MF" fetch "make-$REPO" "v$VER")
+unzip -q "$ARCHIVE" -d "$MF_ROOT/$REPO"
