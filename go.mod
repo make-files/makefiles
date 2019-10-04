2abfc75771a0c0e2083ee23c6cba0b3029e6f684
James Harris <mailjamesharris@gmail.com>
2019-10-04 13:41:48 +1000

    Update module name.

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/make-files/make-go1
+module github.com/make-files/test
 
-go 1.12
+go 1.13
 
 require github.com/golang/protobuf v1.3.2
--- source_test.go
+++ source_test.go
@@ -1,7 +1,7 @@
 package go1_test
 
 import "testing"
-import . "github.com/make-files/make-go1"
+import . "github.com/make-files/test"
 
 func Test(t *testing.T) {
 	Global = &TestMessage{}
