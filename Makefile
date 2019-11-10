eb37e9dd4262fea94888dff56e37e6c0d02ef279
Erin Millard <ezzatron@gmail.com>
2019-11-10 23:38:20 +1000

    Implement a PHPUnit mixin

--- Makefile
+++ Makefile
@@ -1,5 +1,41 @@
+# Always run tests by default, even if other makefiles are included beforehand.
+.DEFAULT_GOAL := test
+
+# PHP_SOURCE_FILES is a space separated list of source files in the repo.
+PHP_SOURCE_FILES += $(shell PATH="$(PATH)" git-find '*.php')
+
+# PHP_TEST_INI_FILE is the path to a PHP INI file that should be used when
+# running tests.
+PHP_TEST_INI_FILE ?= $(shell PATH="$(PATH)" find-file test/php.ini)
+
+# PHP_TEST_ARGS is a set of arguments to pass to PHP when running tests.
+ifeq ($(PHP_TEST_INI_FILE),)
+PHP_TEST_ARGS +=
+else
+PHP_TEST_ARGS += -c "$(PHP_TEST_INI_FILE)"
+endif
+
+# PHP_PHPUNIT_CONFIG_FILE is the path to any existing PHPUnit XML configuration.
+PHP_PHPUNIT_CONFIG_FILE ?= $(shell PATH="$(PATH)" find-file phpunit.xml phpunit.xml.dist)
+
+################################################################################
+
+# _PHP_EXECUTABLES is a space separated list of all executable files in the bin
+# directory.
+_PHP_EXECUTABLES := $(shell find bin -type f -perm +111 2> /dev/null)
+
+# _PHP_TEST_ASSETS is a space separated list of all non-PHP files in the test
+# directory.
+_PHP_TEST_ASSETS := $(shell find test -type f -not -iname "*.php" 2> /dev/null)
+
 # Ensure that dependencies are installed before attempting to build a Docker
 # image.
 DOCKER_BUILD_REQ += vendor
 
+################################################################################
+
 -include .makefiles/pkg/php/v1/composer.mk
+
+ifneq ($(PHP_PHPUNIT_CONFIG_FILE),)
+-include .makefiles/pkg/php/v1/phpunit.mk
+endif
---
+++ phpunit.mk
@@ -0,0 +1,40 @@
+# PHP_PHPUNIT_REQ is a space separated list of prerequisites needed to run the
+# PHPUnit tests.
+PHP_PHPUNIT_REQ += $(PHP_PHPUNIT_CONFIG_FILE) $(PHP_SOURCE_FILES) $(_PHP_EXECUTABLES) $(_PHP_TEST_ASSETS)
+
+################################################################################
+
+# test --- Executes all PHPUnit tests in this package. Stacks with test targets
+# from other test runners.
+.PHONY: test
+test:: $(PHP_PHPUNIT_REQ) | vendor
+	php $(PHP_TEST_ARGS) vendor/bin/phpunit -c $(PHP_PHPUNIT_CONFIG_FILE) --no-coverage
+
+# coverage --- Produces an HTML coverage report. Stacks with coverage targets
+# from other test runners.
+.PHONY: coverage
+coverage:: artifacts/coverage/phpunit/index.html
+
+# coverage-open --- Opens the HTML coverage report in a browser. Stacks with
+# coverage-open targets from other test runners.
+.PHONY: coverage-open
+coverage-open:: artifacts/coverage/phpunit/index.html
+	open "$<"
+
+# prepare --- Perform tasks that need to be executed before committing. Stacks
+# with the "prepare" target form the common makefile.
+.PHONY: prepare
+prepare:: test
+
+# ci --- Builds a machine-readable coverage report. Stacks with the "ci" target
+# from the common makefile.
+.PHONY: ci
+ci:: artifacts/coverage/phpunit/clover.xml
+
+################################################################################
+
+artifacts/coverage/phpunit/index.html: $(PHP_PHPUNIT_REQ) | vendor
+	phpdbg $(PHP_TEST_ARGS) -qrr vendor/bin/phpunit -c $(PHP_PHPUNIT_CONFIG_FILE) --coverage-html="$(@D)"
+
+artifacts/coverage/phpunit/clover.xml: $(PHP_PHPUNIT_REQ) | vendor
+	phpdbg $(PHP_TEST_ARGS) -qrr vendor/bin/phpunit -c $(PHP_PHPUNIT_CONFIG_FILE) --coverage-clover="$@"
