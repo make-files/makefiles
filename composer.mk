336ab97503a7ff9da538c1de46d04e5e1e930a0a
Erin Millard <ezzatron@gmail.com>
2019-11-11 18:23:04 +1000

    Use PHP_ prefix for all variables

--- composer.mk
+++ composer.mk
@@ -1,24 +1,25 @@
-# COMPOSER_INSTALL_ARGS is a set of arguments passed to "composer install"
-COMPOSER_INSTALL_ARGS ?= --no-suggest --optimize-autoloader --prefer-dist
+# PHP_COMPOSER_INSTALL_ARGS is a set of arguments passed to "composer install"
+PHP_COMPOSER_INSTALL_ARGS ?= --no-suggest --optimize-autoloader --prefer-dist
 
 # The type of package as defined in the composer.json "type" property
-COMPOSER_PACKAGE_TYPE := $(shell $(MF_ROOT)/pkg/php/v1/bin/composer-package-type)
+PHP_COMPOSER_PACKAGE_TYPE := $(shell $(MF_ROOT)/pkg/php/v1/bin/composer-package-type)
 
 # Whether this package is intended to be published
-ifeq ($(COMPOSER_PACKAGE_TYPE),library)
-COMPOSER_PUBLISH ?= true
+ifeq ($(PHP_COMPOSER_PACKAGE_TYPE),library)
+PHP_COMPOSER_PUBLISH ?= true
 else
-COMPOSER_PUBLISH ?=
+PHP_COMPOSER_PUBLISH ?=
 endif
 
-# COMPOSER_VALIDATE_PUBLISH_ARGS and COMPOSER_VALIDATE_NO_PUBLISH_ARGS are
-# arguments passed to "composer validate"
-COMPOSER_VALIDATE_PUBLISH_ARGS    ?= --strict
-COMPOSER_VALIDATE_NO_PUBLISH_ARGS ?= --no-check-publish
+# PHP_COMPOSER_VALIDATE_PUBLISH_ARGS and PHP_COMPOSER_VALIDATE_NO_PUBLISH_ARGS
+# are arguments passed to "composer validate"
+PHP_COMPOSER_VALIDATE_PUBLISH_ARGS    ?= --strict
+PHP_COMPOSER_VALIDATE_NO_PUBLISH_ARGS ?= --no-check-publish
 
 ################################################################################
 
-# Ensure that dependencies are installed before attempting to build a Docker image.
+# Ensure that dependencies are installed before attempting to build a Docker
+# image.
 DOCKER_BUILD_REQ += composer.json composer.lock
 
 ################################################################################
@@ -31,16 +32,16 @@ prepare:: artifacts/composer/validate.touch
 ################################################################################
 
 vendor: composer.lock
-	composer install $(COMPOSER_INSTALL_ARGS)
+	composer install $(PHP_COMPOSER_INSTALL_ARGS)
 
 composer.lock: | composer.json
-	composer install $(COMPOSER_INSTALL_ARGS)
+	composer install $(PHP_COMPOSER_INSTALL_ARGS)
 
 composer.json:
 	composer init --no-interaction
 
 artifacts/composer/validate.touch: composer.json
-	$(eval ARGS := $(if $(COMPOSER_PUBLISH),$(COMPOSER_VALIDATE_PUBLISH_ARGS),$(COMPOSER_VALIDATE_NO_PUBLISH_ARGS)))
+	$(eval ARGS := $(if $(PHP_COMPOSER_PUBLISH),$(PHP_COMPOSER_VALIDATE_PUBLISH_ARGS),$(PHP_COMPOSER_VALIDATE_NO_PUBLISH_ARGS)))
 	composer validate $(ARGS)
 
 	@mkdir -p "$(@D)"
