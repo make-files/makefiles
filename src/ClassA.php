ac8f87b73159cbca62208a5263a1c325554c3012
Erin Millard <ezzatron@gmail.com>
2019-11-11 23:07:02 +1000

    Add test suite

---
+++ src/ClassA.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Makefiles\Test;
+
+class ClassA
+{
+    public static function methodA ()
+    {
+        return 'a';
+    }
+
+    public static function methodB ()
+    {
+        return 'b';
+    }
+}
---
+++ test/IniTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Makefiles\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class IniTest extends TestCase
+{
+    public function testIni ()
+    {
+        $this->assertSame('1', get_cfg_var('makefiles.dev'));
+    }
+}
