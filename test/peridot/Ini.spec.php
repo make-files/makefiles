ed12a02bd79c6d6e00be85cb5d0b5d6514a38ee4
Erin Millard <ezzatron@gmail.com>
2019-12-01 16:18:23 +1000

    Remove INI tests

--- test/peridot/Ini.spec.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Makefiles\Test;
-
-describe('INI file', function () {
-    it('should read the correct file', function () {
-        expect(get_cfg_var('makefiles.dev.peridot'))->to->equal('1');
-    });
-});
