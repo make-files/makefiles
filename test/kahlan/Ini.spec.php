d890177b4f3a8708c86f016626f476e3cd72a1f3
Erin Millard <ezzatron@gmail.com>
2019-12-01 16:17:43 +1000

    Remove INI tests

--- test/kahlan/Ini.spec.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Makefiles\Test;
-
-describe('INI file', function () {
-    it('should read the correct file', function () {
-        expect(get_cfg_var('makefiles.dev.kahlan'))->toBe('1');
-    });
-});
