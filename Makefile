440f778783967b77eb6aaee7bb3bcd10146825d3
Erin Millard <ezzatron@gmail.com>
2020-01-10 14:55:04 +1000

    Implement Jest

--- Makefile
+++ Makefile
@@ -4,7 +4,14 @@
 # JS_SOURCE_FILES is a space separated list of source files in the repo.
 JS_SOURCE_FILES += $(shell PATH="$(PATH)" git-find '*.js')
 
+# JS_JEST_CONFIG_FILE is the path to any existing Jest configuration.
+JS_JEST_CONFIG_FILE ?= $(shell PATH="$(PATH)" find-first-matching-file jest.config.js jest.config.cjs jest.config.json)
+
 ################################################################################
+
+# _JS_REQ is a space separated list of automatically detected prerequisites
+# needed to run JavaScript targets.
+_JS_REQ += $(GENERATED_FILES)
 
 # _JS_TEST_ASSETS is a space separated list of all non-JS files in the test
 # directory.
@@ -13,3 +20,9 @@ _JS_TEST_ASSETS := $(shell find test -type f -not -iname "*.js" 2> /dev/null)
 # Ensure that dependencies are installed before attempting to build a Docker
 # image.
 DOCKER_BUILD_REQ += node_modules
+
+################################################################################
+
+ifneq ($(JS_JEST_CONFIG_FILE),)
+-include .makefiles/pkg/js/v1/jest.mk
+endif
---
+++ jest.mk
@@ -0,0 +1,64 @@
+# JS_JEST_REQ is a space separated list of prerequisites needed to run the Jest
+# tests.
+JS_JEST_REQ +=
+
+################################################################################
+
+# _JS_JEST_REQ is a space separated list of automatically detected
+# prerequisites needed to run the Jest tests.
+_JS_JEST_REQ += node_modules $(JS_JEST_CONFIG_FILE) $(JS_SOURCE_FILES) $(_JS_TEST_ASSETS) $(_JS_REQ)
+
+# _JS_JEST_ARGS is a set of arguments to use for every execution of Jest.
+_JS_JEST_ARGS := --config "$(JS_JEST_CONFIG_FILE)"
+
+################################################################################
+
+# test --- Executes all tests.
+.PHONY: test
+test:: test-jest
+
+# coverage --- Executes all tests, producing coverage reports.
+.PHONY: coverage
+coverage:: coverage-jest
+
+# coverage-open --- Opens all HTML coverage reports in a browser.
+.PHONY: coverage-open
+coverage-open:: coverage-jest-open
+
+# ci --- Perform tasks that should be run as part of continuous integration.
+.PHONY: ci
+ci:: ci-jest
+
+################################################################################
+
+# test-jest --- Executes all Jest tests in this package.
+.PHONY: test-jest
+test-jest: artifacts/test/jest.touch
+
+# coverage-jest --- Produces a Jest HTML coverage report.
+.PHONY: coverage-jest
+coverage-jest: artifacts/coverage/jest/index.html
+
+# coverage-jest-open --- Opens the Jest HTML coverage report in a browser.
+.PHONY: coverage-jest-open
+coverage-jest-open: artifacts/coverage/jest/index.html
+	open "$<"
+
+# ci-jest --- Executes all Jest tests in this package, and produces a
+#             machine-readable coverage report.
+.PHONY: ci-jest
+ci-jest: artifacts/coverage/jest/lcov.info
+
+################################################################################
+
+artifacts/coverage/jest/index.html: $(JS_JEST_REQ) $(_JS_JEST_REQ)
+	node_modules/.bin/jest $(_JS_JEST_ARGS) --coverage --coverage-directory="$(@D)" --coverage-reporters text-summary --coverage-reporters html
+
+artifacts/coverage/jest/lcov.info: $(JS_JEST_REQ) $(_JS_JEST_REQ)
+	node_modules/.bin/jest $(_JS_JEST_ARGS) --ci --coverage --coverage-directory="$(@D)" --coverage-reporters text-summary --coverage-reporters lcovonly
+
+artifacts/test/jest.touch: $(JS_JEST_REQ) $(_JS_JEST_REQ)
+	node_modules/.bin/jest $(_JS_JEST_ARGS)
+
+	@mkdir -p "$(@D)"
+	@touch "$@"
