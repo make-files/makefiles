d569cd9237dccc29483920b956f95d6284276311
Erin Millard <ezzatron@gmail.com>
2021-10-26 15:13:38 +1000

    Add support for pnpm

---
+++ pkg/js/v1/with-pnpm.mk
@@ -0,0 +1,39 @@
+# JS_PNPM_INSTALL_ARGS is a set of arguments passed to "pnpm install"
+JS_PNPM_INSTALL_ARGS ?=
+
+ifneq ($(MF_NON_INTERACTIVE),)
+	JS_PNPM_INSTALL_ARGS += --non-interactive --no-progress
+endif
+
+################################################################################
+
+# set-package-version --- Sets the version field in package.json to a semver
+# representation of the HEAD commit.
+.PHONY: set-package-version
+set-package-version:
+	pnpm version --no-git-tag-version "$(SEMVER)"
+
+################################################################################
+
+node_modules: package.json
+ifeq ($(wildcard pnpm-lock.yaml),)
+	pnpm install $(JS_PNPM_INSTALL_ARGS)
+else
+	pnpm install $(JS_PNPM_INSTALL_ARGS) --frozen-lockfile
+endif
+
+	@touch "$@"
+
+package.json:
+ifeq ($(wildcard package.json),)
+	cp "$(MF_ROOT)/pkg/js/v1/etc/init.package.json" "$(MF_PROJECT_ROOT)/package.json"
+endif
+
+artifacts/pnpm/production/node_modules: package.json
+ifeq ($(wildcard pnpm-lock.yaml),)
+	pnpm install $(JS_PNPM_INSTALL_ARGS) --prod --modules-dir "$@"
+else
+	pnpm install $(JS_PNPM_INSTALL_ARGS) --prod --modules-dir "$@" --frozen-lockfile
+endif
+
+	@touch "$@"
