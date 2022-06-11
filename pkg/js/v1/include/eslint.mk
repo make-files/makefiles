3f244f808afac8deb4b05bcd32a910f5563a0ca5
Erin Millard <ezzatron@gmail.com>
2022-06-11 11:10:18 +1000

    Simplify ESLint targets, make them PHONY

--- pkg/js/v1/include/eslint.mk
+++ pkg/js/v1/include/eslint.mk
@@ -5,7 +5,7 @@ JS_ESLINT_REQ +=
 
 # _JS_ESLINT_REQ is a space separated list of automatically detected
 # prerequisites needed to run ESLint.
-_JS_ESLINT_REQ += node_modules $(JS_ESLINT_CONFIG_FILE) $(JS_SOURCE_FILES) $(JS_TEST_FILES) $(GENERATED_FILES)
+_JS_ESLINT_REQ += node_modules
 
 # _JS_ESLINT_CACHE_FILE is a path to the cache file to use when running ESLint.
 _JS_ESLINT_CACHE_FILE := artifacts/lint/eslint/cache
@@ -21,11 +21,11 @@ lint:: eslint
 
 # precommit --- Perform tasks that need to be executed before committing.
 .PHONY: precommit
-precommit:: artifacts/lint/eslint/precommit.touch
+precommit:: eslint-strict
 
 # ci --- Perform tasks that should be run as part of continuous integration.
 .PHONY: ci
-ci:: artifacts/lint/eslint/ci.touch
+ci:: eslint-check
 
 ################################################################################
 
@@ -33,24 +33,18 @@ ci:: artifacts/lint/eslint/ci.touch
 #            automatically where possible. Warnings will not cause this target
 #            to fail, but errors will.
 .PHONY: eslint
-eslint: artifacts/lint/eslint/fix.touch
-
-################################################################################
-
-artifacts/lint/eslint:
-	@mkdir -p "$@"
-
-artifacts/lint/eslint/ci.touch: artifacts/lint/eslint $(JS_ESLINT_REQ) $(_JS_ESLINT_REQ)
-	node_modules/.bin/eslint $(_JS_ESLINT_ARGS) --max-warnings 0 "$(MF_PROJECT_ROOT)"
-
-	@touch "$@"
-
-artifacts/lint/eslint/fix.touch: artifacts/lint/eslint $(JS_ESLINT_REQ) $(_JS_ESLINT_REQ)
+eslint: $(JS_ESLINT_REQ) $(_JS_ESLINT_REQ)
 	node_modules/.bin/eslint $(_JS_ESLINT_ARGS) --fix "$(MF_PROJECT_ROOT)"
 
-	@touch "$@"
-
-artifacts/lint/eslint/precommit.touch: artifacts/lint/eslint $(JS_ESLINT_REQ) $(_JS_ESLINT_REQ)
+# eslint-strict --- Same as the eslint target, but ESLint warnings will cause
+#                   failures.
+.PHONY: eslint-strict
+eslint-strict: $(JS_ESLINT_REQ) $(_JS_ESLINT_REQ)
 	node_modules/.bin/eslint $(_JS_ESLINT_ARGS) --fix --max-warnings 0 "$(MF_PROJECT_ROOT)"
 
-	@touch "$@"
+# eslint-check --- Same as the eslint-strict target, but issues that could
+#                  automatically be fixed will instead cause this target to
+#                  fail.
+.PHONY: eslint-check
+eslint-check: $(JS_ESLINT_REQ) $(_JS_ESLINT_REQ)
+	node_modules/.bin/eslint $(_JS_ESLINT_ARGS) --max-warnings 0 "$(MF_PROJECT_ROOT)"
