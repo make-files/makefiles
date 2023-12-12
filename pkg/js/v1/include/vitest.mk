5c464197d7f2db799ee647f65f94f65955d54c00
Erin Millard <contact@erin.id.au>
2023-12-12 10:29:16 +1000

    Add Vitest support to the `js` package

---
+++ pkg/js/v1/include/vitest.mk
@@ -0,0 +1,72 @@
+# JS_VITEST_REQ is a space separated list of prerequisites needed to run the
+# Vitest tests.
+JS_VITEST_REQ +=
+
+################################################################################
+
+# _JS_VITEST_REQ is a space separated list of automatically detected
+# prerequisites needed to run the Vitest tests.
+_JS_VITEST_REQ += artifacts/link-dependencies.touch $(GENERATED_FILES)
+
+# _JS_VITEST_ARGS is a set of arguments to use for every execution of Vitest.
+_JS_VITEST_ARGS := --config "$(JS_VITEST_CONFIG_FILE)"
+
+################################################################################
+
+# test --- Executes all tests.
+.PHONY: test
+test:: vitest
+
+# coverage --- Executes all tests, producing coverage reports.
+.PHONY: coverage
+coverage:: vitest-coverage
+
+# coverage-open --- Opens all HTML coverage reports in a browser.
+.PHONY: coverage-open
+coverage-open:: vitest-coverage-open
+
+# precommit --- Perform tasks that need to be executed before committing.
+.PHONY: precommit
+precommit:: vitest
+
+# ci --- Perform tasks that should be run as part of continuous integration.
+.PHONY: ci
+ci:: vitest-coverage-lcov
+
+################################################################################
+
+# vitest --- Executes all Vitest tests in this package.
+.PHONY: vitest
+vitest: $(JS_VITEST_REQ) $(_JS_VITEST_REQ)
+	$(JS_EXEC) vitest run $(_JS_VITEST_ARGS)
+
+# vitest-coverage --- Produces a Vitest HTML coverage report.
+.PHONY: vitest-coverage
+vitest-coverage: artifacts/coverage/vitest/index.html
+
+# vitest-coverage-open --- Opens the Vitest HTML coverage report in a browser.
+.PHONY: vitest-coverage-open
+vitest-coverage-open: artifacts/coverage/vitest/index.html
+	$(MF_BROWSER) "$<"
+
+# vitest-coverage-lcov --- Produces a Vitest LCOV coverage report.
+.PHONY: vitest-coverage-lcov
+vitest-coverage-lcov: artifacts/coverage/vitest/lcov.info
+
+################################################################################
+
+.PHONY: artifacts/coverage/vitest/index.html # always rebuild
+artifacts/coverage/vitest/index.html: $(JS_VITEST_REQ) $(_JS_VITEST_REQ)
+	$(JS_EXEC) vitest run $(_JS_VITEST_ARGS) \
+		--coverage.enabled \
+		--coverage.reportsDirectory="$(@D)" \
+		--coverage.reporter=text \
+		--coverage.reporter=html
+
+.PHONY: artifacts/coverage/vitest/lcov.info # always rebuild
+artifacts/coverage/vitest/lcov.info: $(JS_VITEST_REQ) $(_JS_VITEST_REQ)
+	$(JS_EXEC) vitest run $(_JS_VITEST_ARGS) \
+		--coverage.enabled \
+		--coverage.reportsDirectory="$(@D)" \
+		--coverage.reporter=text \
+		--coverage.reporter=lcovonly
