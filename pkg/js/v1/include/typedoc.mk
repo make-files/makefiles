2f7f0f608c7892ccb0a6be927baae44f556e5964
Erin Millard <contact@erin.id.au>
2024-01-15 16:02:42 +1000

    Add TypeDoc support.

---
+++ pkg/js/v1/include/typedoc.mk
@@ -0,0 +1,27 @@
+# JS_TYPEDOC_REQ is a space separated list of prerequisites needed to run
+# TypeDoc.
+JS_TYPEDOC_REQ +=
+
+################################################################################
+
+# _JS_TYPEDOC_REQ is a space separated list of automatically detected
+# prerequisites needed to run TypeDoc.
+_JS_TYPEDOC_REQ += artifacts/link-dependencies.touch
+
+################################################################################
+
+# typedoc --- Generate TypeDoc documentation.
+.PHONY: typedoc
+typedoc: artifacts/docs/typedoc
+
+# typedoc-open --- Open TypeDoc documentation.
+.PHONY: typedoc-open
+typedoc-open: artifacts/docs/typedoc
+	$(MF_BROWSER) "$<"
+
+################################################################################
+
+artifacts/docs/typedoc: $(JS_TYPEDOC_REQ) $(_JS_TYPEDOC_REQ)
+	@rm -rf "$@"
+	$(JS_EXEC) typedoc --options "$(JS_TYPEDOC_CONFIG_FILE)"
+	@touch "$@"
