db0df6b288b87e86609934515ecda53204923356
Erin Millard <contact@erin.id.au>
2024-06-25 17:05:52 +1000

    Add TypeDoc linting targets

--- pkg/js/v1/include/typedoc.mk
+++ pkg/js/v1/include/typedoc.mk
@@ -10,6 +10,20 @@ _JS_TYPEDOC_REQ += artifacts/link-dependencies.touch $(JS_TYPEDOC_CONFIG_FILE) $
 
 ################################################################################
 
+# lint --- Check for syntax, configuration, code style and/or formatting issues.
+.PHONY: lint
+lint:: typedoc-check
+
+# precommit --- Perform tasks that need to be executed before committing.
+.PHONY: precommit
+precommit:: typedoc-check
+
+# ci --- Perform tasks that should be run as part of continuous integration.
+.PHONY: ci
+ci:: typedoc-check
+
+################################################################################
+
 # typedoc --- Generate TypeDoc documentation.
 .PHONY: typedoc
 typedoc: artifacts/docs/typedoc
@@ -18,6 +32,14 @@ typedoc: artifacts/docs/typedoc
 .PHONY: typedoc-open
 typedoc-open: artifacts/docs/typedoc
 	$(MF_BROWSER) "$(<)/index.html"
+
+# typedoc-check --- Check for TypeDoc documentation issues.
+.PHONY: typedoc-check
+typedoc-check: $(JS_TYPEDOC_REQ) $(_JS_TYPEDOC_REQ)
+	$(JS_EXEC) typedoc \
+		--options "$(JS_TYPEDOC_CONFIG_FILE)" \
+		--emit none \
+		--treatWarningsAsErrors
 
 ################################################################################
 
