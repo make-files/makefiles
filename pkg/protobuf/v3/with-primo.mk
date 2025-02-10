a1380cff4f0eab0151440d183dd8ac64aaede6d9
Erin Millard <contact@erin.id.au>
2025-02-11 09:30:19 +1000

    Replace instances of v2 with v3

--- pkg/protobuf/v3/with-primo.mk
+++ pkg/protobuf/v3/with-primo.mk
@@ -1,7 +1,7 @@
 GENERATED_FILES += $(foreach f,$(PROTO_FILES:.proto=_primo.pb.go),$(if $(findstring /_,/$f),,$f))
 
 artifacts/protobuf/bin/protoc-gen-go-primo: go.mod
-	$(MF_ROOT)/pkg/protobuf/v2/bin/install-protoc-gen-go-primo "$(MF_PROJECT_ROOT)/$(@D)"
+	$(MF_ROOT)/pkg/protobuf/v3/bin/install-protoc-gen-go-primo "$(MF_PROJECT_ROOT)/$(@D)"
 
 %_primo.pb.go: %.proto artifacts/protobuf/bin/protoc-gen-go-primo artifacts/protobuf/bin/run-protoc artifacts/protobuf/args/common artifacts/protobuf/args/go-primo
 	$(PROTO_PROTOC_PATH) \
@@ -14,4 +14,4 @@ artifacts/protobuf/args/go-primo: go.mod
 	echo "--plugin protoc-gen-go-primo=artifacts/protobuf/bin/protoc-gen-go-primo" > "$@"
 	echo "--go-primo_opt=module=$$(go list -m)" >> "$@"
 	echo "--go-primo_out=." >> "$@"
-	$(MF_ROOT)/pkg/protobuf/v2/bin/generate-include-paths >> "$@"
+	$(MF_ROOT)/pkg/protobuf/v3/bin/generate-include-paths >> "$@"
