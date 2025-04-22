d55854ee6103cd36866417cda6b97154a7542bc6
Erin Millard <contact@erin.id.au>
2025-04-22 14:45:19 +1000

    Avoid bash syntax in list-generated

--- lib/core/include/common.mk
+++ lib/core/include/common.mk
@@ -172,7 +172,7 @@ verify-generated:
 # tracked by git.
 .PHONY: list-generated
 list-generated:
-	@if [[ -n "$(GENERATED_FILES)" ]]; then git ls-files $(GENERATED_FILES); else true; fi
+	@if [ -n "$(GENERATED_FILES)" ]; then git ls-files $(GENERATED_FILES); else true; fi
 
 # test --- Executes all tests.
 # Individual language Makefiles are expected to add additional recipies for this
