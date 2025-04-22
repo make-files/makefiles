dcf2b73c495536b23da58f7fee367d52daddd6b2
Erin Millard <contact@erin.id.au>
2025-04-22 14:27:19 +1000

    Eval GENERATED_FILES at run time in list-generated

--- lib/core/include/common.mk
+++ lib/core/include/common.mk
@@ -172,11 +172,7 @@ verify-generated:
 # tracked by git.
 .PHONY: list-generated
 list-generated:
-ifeq ($(GENERATED_FILES),)
-	@true
-else
-	@git ls-files $(GENERATED_FILES)
-endif
+	@if [[ -n "$(GENERATED_FILES)" ]]; then git ls-files $(GENERATED_FILES); else true; fi
 
 # test --- Executes all tests.
 # Individual language Makefiles are expected to add additional recipies for this
