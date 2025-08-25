2aca327a2502b30ed2c6dc3e9687135e48da60cb
James Harris <contact@jamesharris.id.au>
2025-08-26 09:23:12 +1000

    Fix use of `artifacts/protobuf/args/go-primo` in argument list.

--- pkg/protobuf/v2/with-primo.mk
+++ pkg/protobuf/v2/with-primo.mk
@@ -4,7 +4,7 @@ artifacts/protobuf/bin/protoc-gen-go-primo: go.mod
 	$(MF_ROOT)/pkg/protobuf/v2/bin/install-protoc-gen-go-primo "$(MF_PROJECT_ROOT)/$(@D)"
 
 %_primo.pb.go: %.proto artifacts/protobuf/bin/protoc-gen-go-primo artifacts/protobuf/bin/run-protoc artifacts/protobuf/args/go-primo
-	$(PROTO_PROTOC_PATH) $$(artifacts/protobuf/args/go-primo) $(MF_PROJECT_ROOT)/$<
+	$(PROTO_PROTOC_PATH) $$(cat artifacts/protobuf/args/go-primo) $(MF_PROJECT_ROOT)/$<
 	$(MF_ROOT)/pkg/protobuf/v2/bin/revert-version-only-changes $@
 
 artifacts/protobuf/args/go-primo: artifacts/protobuf/args/go-common
